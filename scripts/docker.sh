3e3892a357fc1ed102471c356581b4367d38e20e
unknown <msgtokumar@gmail.com>
2018-12-07 08:34:08 +1100

    first commit

---
+++ scripts/docker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Allow toggling components to install and update based off flags
+updatedocker=1
+updatedockercompose=1
+
+dockercomposeversion="1.7.1"
+
+echo ""
+echo "Updating Docker($updatedocker)"
+echo "Updating Docker Compose($updatedockercompose)"
+
+echo ""
+echo "Starting Install"
+echo ""
+
+# add docker group and add ubuntu to it
+echo "hello"
+
+
+
